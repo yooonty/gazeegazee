661242c4ed437f626ab51e336967cde40061bf0c
Yoon Tae In <yte0130@gmail.com>
2023-06-01 17:14:42 +0900

    Feat : 페이징 시 상품 갯수

--- gazee/src/main/java/com/multi/gazee/product/PageVO.java
+++ gazee/src/main/java/com/multi/gazee/product/PageVO.java
@@ -4,14 +4,15 @@ public class PageVO {
 	private int start;
 	private int end;
 	private int page;
+	private int num; // 한 페이지당 상품 갯수
 
 	public void setStartEnd(int page) {
 		// page별로 start, end값만 구해주면 됨.
-		start = 1 + (page - 1) * 20;
+		start = 1 + (page - 1) * num;
 		// 무조건 1부터 시작
 		// 1page: 1 + 0 * 10 => start 1
 		// 2page: 1 + 1 * 10 => start 11
-		end = page * 20;
+		end = page * num;
 		// 1page: 1 * 10 => end 10
 		// 2page: 2 * 10 => end 20
 	}
@@ -39,9 +40,18 @@ public class PageVO {
 	public void setEnd(int end) {
 		this.end = end;
 	}
+	
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
 
 	@Override
 	public String toString() {
-		return "PageVO [start=" + start + ", end=" + end + ", page=" + page + "]";
+		return "PageVO [start=" + start + ", end=" + end + ", page=" + page + ", num=" + num + "]";
 	}
+
 }
